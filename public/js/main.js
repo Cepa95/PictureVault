a674e62e1a4568c899724cdec2eab90fc2ab4a1d
Cepa95 <ceprnic.josip@gmail.com>
2023-10-20 21:40:10 +0200

    styling and navigation

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+ // JavaScript code for displaying the selected image and validating the form
+ const selectedImage = document.getElementById('selectedImage');
+ const imagePreview = document.getElementById('imagePreview');
+ const imageInput = document.getElementById('imageInput');
+ const errorMessage = document.getElementById('errorMessage');
+
+ imageInput.addEventListener('change', function (e) {
+   if (e.target.files.length > 0) {
+     const file = e.target.files[0];
+     const reader = new FileReader();
+
+     reader.onload = function (event) {
+       selectedImage.src = event.target.result;
+       imagePreview.style.display = 'block'; // Show the image preview
+     };
+
+     reader.readAsDataURL(file);
+   } else {
+     selectedImage.src = '';
+     imagePreview.style.display = 'none';
+   }
+ });
+
+ function validateForm() {
+   if (imageInput.files.length === 0) {
+     errorMessage.style.display = 'block';
+     return false; // Prevent form submission if no image is selected
+   }
+   errorMessage.style.display = 'none';
+   return true; // Allow form submission if an image is selected
+ }
